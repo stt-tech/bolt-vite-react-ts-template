98f38129ea2b2fa170c8ebe481d9a8258802a97b
Alpha <suntingting28@meituan.com>
2025-03-13 11:40:02 +0800

    添加publish baseurlC

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PUBLIC_PATH = process.env.PUBLIC_PATH
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: PUBLIC_PATH,
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
